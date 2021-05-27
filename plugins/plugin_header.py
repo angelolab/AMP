10add8ee9cb131761793294645a75e33260033fa
ackagel <ackagel@ucdavis.edu>
2021-05-27 13:31:42 -0700

    updates plugin header and setup script

--- plugins/plugin_header.py
+++ plugins/plugin_header.py
@@ -1,10 +1,18 @@
 from PyQt5 import QtWidgets, QtCore, uic
 
 from amp.main_viewer import MainViewer
-from amp.mplwidget import ImagePlot
+from amp.mplwidget import ImagePlot, HistPlot, Plot
 
 import numpy as np
 from scipy.ndimage import gaussian_filter
+from scipy.stats import gamma
+from scipy.optimize import fsolve
+from scipy.special import digamma
+from scipy.special import gamma as gamma_func
+from sklearn.neighbors import NearestNeighbors
+
+# extra hidden import (should tell pyinstaller to update their sklearn hook)
+import sklearn.utils._weight_vector
 
 import os
 import shutil
